892cf90b8da3e5f9d624a36d25e171fb4598313e
siditom <tomer.cd@gmail.com>
2018-03-25 11:01:10 +0300

    Added CASP13 basic score functions - CASP13_LegacyScore and CASP13_Score

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+currentPath=`pwd -P`
+sed -i 's@ .*\/aux@ '"${currentPath}"'\/aux@g' commands
+sed -i 's@ .*\/parameters@ '"${currentPath}"'\/parameters@g' commands
+sed -i 's@ .*\/score_functions@ '"${currentPath}"'\/score_functions@g' commands
+# sed -i 's@APP_PATH=.*@APP_PATH='"${currentPath}"'@g' ./score.sh
